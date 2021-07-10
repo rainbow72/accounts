2027af3619f35cc3ab2036302ae37ed7535d5339
golf.athlete <fortechachademy@gmail.com>
2021-07-10 23:32:11 +0000

    application is work well w/o new transaction function

--- app/controllers/interrock_controller.rb
+++ app/controllers/interrock_controller.rb
@@ -1,8 +1,11 @@
 class InterrockController < ApplicationController
   def index
+    @categories = Category.all
+    @subcategories = Subcategory.none
   end
 
   def subcategories
-    @subcategories = Subcategory.where(category_id: params[:category_id])
+    @subcategories = Subcategory.where(category_id: params[:category_id]).pluck(:name, :id)
+    @subcategories.unshift(["",""])
   end
 end
